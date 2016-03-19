6f17737740890a883841f4d038ead16929ca4555
vayn <joheandroid@gmail.com>
2016-03-18 22:21:22 -0430

    Empezando y agrado de estatus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ojalabook::Application.config.secret_token = '9fc1808ce2b96f8e7bacdac39bfaf66b5762b432f619e1d511f7d56b1781877ea9eff64f180ed5ee8513428054f7e600c3f519217335d4a161f60f37d8848a87'
